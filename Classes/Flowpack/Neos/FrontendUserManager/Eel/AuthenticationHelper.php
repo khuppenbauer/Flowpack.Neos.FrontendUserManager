b03b282f5529199307a9b140d6a326bbdba00fd5
Kerstin Huppenbauer <k.huppenbauer@web.de>
2014-10-04 19:47:32 +0200

    [TASK] Initial commit

---
+++ Classes/Flowpack/Neos/FrontendUserManager/Eel/AuthenticationHelper.php
@@ -0,0 +1,78 @@
+<?php
+namespace Flowpack\Neos\FrontendUserManager\Eel;
+
+
+/*                                                                                                  *
+ * This script belongs to the TYPO3 Flow package "Flowpack.ElasticSearch.ContentRepositoryAdaptor". *
+ *                                                                                                  *
+ * It is free software; you can redistribute it and/or modify it under                              *
+ * the terms of the GNU Lesser General Public License, either version 3                             *
+ *  of the License, or (at your option) any later version.                                          *
+ *                                                                                                  *
+ * The TYPO3 project - inspiring people to share!                                                   *
+ *                                                                                                  */
+
+use TYPO3\Eel\ProtectedContextAwareInterface;
+use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Flow\Security\Context;
+
+/**
+ * Eel Helper to get Authentication Information in TypoScript
+ */
+class AuthenticationHelper implements ProtectedContextAwareInterface {
+
+	/**
+	 * @var Context
+	 */
+	protected $securityContext;
+
+	/**
+	 * Injects the Security Context
+	 *
+	 * @param Context $securityContext
+	 * @return void
+	 */
+	public function injectSecurityContext(Context $securityContext) {
+		$this->securityContext = $securityContext;
+	}
+
+	/**
+	 * Returns the Users Fullname or the Account Identifier
+	 *
+	 * @return string the rendered string
+	 */
+	public function identifier() {
+		$user = $this->securityContext->getPartyByType('TYPO3\Neos\Domain\Model\User');
+		/** @var  \TYPO3\Neos\Domain\Model\User $user */
+		if ($user !== NULL) {
+			return ($user->getName() !== NULL) ? $user->getName()->getFullname() : $this->securityContext->getAccount()->getAccountIdentifier();
+		}
+	}
+
+	/**
+	 * Checks whether a User is authenticated
+	 *
+	 * @param string $providerName
+	 * @return boolean
+	 * @api
+	 */
+	public function isAuthenticated($providerName) {
+		$activeTokens = $this->securityContext->getAuthenticationTokens();
+		/** @var $token TokenInterface */
+		foreach ($activeTokens as $token) {
+			if ($token->getAuthenticationProviderName() === $providerName && $token->isAuthenticated()) {
+				return TRUE;
+			}
+		}
+	}
+
+	/**
+	 * All methods are considered safe
+	 *
+	 * @param string $methodName
+	 * @return boolean
+	 */
+	public function allowsCallOfMethod($methodName) {
+		return TRUE;
+	}
+}
